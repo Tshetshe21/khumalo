45c4b950086fdf8a6f330cd2336dbacc7d944a75
Tshetshe21 <124685461+Tshetshe21@users.noreply.github.com>
2023-06-19 12:11:19 +0200

    first commit

---
+++ khumalo/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/hero'
+
+import HeroSection from './components/hero'
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection/> 
+    
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ khumalo/src/components/hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Welcome to My Website</h1>
+        <p>Discover the world of ReactJS!</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
